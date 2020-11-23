15cba9a9833b538b56c4941b3dcca138dc97b818
_TCHNX <patterto@mail.gvsu.edu>
2020-11-23 18:02:33 -0500

    Completed

---
+++ fitness/scripts/myFunctions.js
@@ -0,0 +1,22 @@
+/* Tonae Patterson 
+November 18 2020
+My Functions List 
+*/
+
+// Thank you alert
+function tyAlert() {
+    alert("Thank you for your interest in our fitness club. We will contact you soon.");
+}
+
+// Plank video window
+function plank() {
+    window.open("scripts/plank.html", "_blank", "width=610, height=360");
+}
+// Burpee video window
+function burpees() {
+    window.open("scripts/burpees.html", "_blank", "width=610, height=360");
+}
+// Mountain climber video window
+function mtnClimber() {
+    window.open("scripts/mtn.html", "_blank", "width=610, height=360");
+}
